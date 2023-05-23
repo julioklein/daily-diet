c719368c4019640a35e8e019b32ed434e5076827
Julio Cesar <julio.ckcalista@gmail.com>
2023-05-22 22:24:21 -0300

    chore: add phosphor icons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,12 @@
 import * as S from './styles';
+import { Airplane } from 'phosphor-react-native';
+
 
 export const Home = () => {
   return (
     <S.Container>
       <S.ScreenTitle>Home</S.ScreenTitle>
+      <Airplane color='green' size={48} />
     </S.Container>
   );
 };
