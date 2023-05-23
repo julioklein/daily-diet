a4a8746472d4d2f3b17dfca399a9137b91bbfb18
Julio Cesar <julio.ckcalista@gmail.com>
2023-05-22 22:31:52 -0300

    chore: add project assets

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,13 @@
+import { Image } from 'react-native';
 import * as S from './styles';
 import { Airplane } from 'phosphor-react-native';
 
+const Logo = require('assets/logo/Logo.png');
 
 export const Home = () => {
   return (
     <S.Container>
+      <Image source={Logo} />
       <S.ScreenTitle>Home</S.ScreenTitle>
       <Airplane color='green' size={48} />
     </S.Container>
